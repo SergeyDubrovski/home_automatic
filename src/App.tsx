af07da0f474a2ec10310faf6adb1dd2af13a7ecc
SergeyDubrovski <sovelectron@rambler.ru>
2025-01-16 21:23:13 +0300

    2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 import './App.css'
 import Home from './Home'
 
