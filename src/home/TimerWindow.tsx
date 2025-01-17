73a6825542c314d6b210713f74122ba74c13e655
SergeyDubrovski <sovelectron@gmail.com>
2025-01-17 23:30:18 +0300

    2

--- src/home/TimerWindow.tsx
+++ src/home/TimerWindow.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 import s from './home.module.css'
+import { Switch } from '@mui/material'
 
 type Props = {
     turnOnOff: (onOff: boolean) => void
@@ -11,17 +12,44 @@ type Props = {
 function TimerWindow({ turnOnOff, showTimer }: Props) {
 
 
+const [time, setTime] = useState<Array<boolean>>([
+    false, false, true, false, false
+])
+const [handleTime, setHandleTime] = useState<number>(1)
     const onClick = () => {
+        console.log(handleTime);
+        
         turnOnOff(true)
         showTimer()
     }
+  const timeString: Array<string> = ['1 min', '5 min', '10 min', '30 min', 'forever']  
+
+  const onChange = (index:number) => {
+    setHandleTime(index)
+    setTime(
+        prev => prev.map((_, i) => {
+            if(i === index) return true
+            return false
+        })
+    )
+  }
+  
+
+    const switchCheck: JSX.Element[] = timeString.map((el, i) => {
+        return (
+            <div key={el}>
+                    <Switch checked={time[i] ? true : false} color="warning" 
+                    onChange={() => {onChange(i)}}/>
+                    <span>{el}</span>
+                </div>
+        )
+    })
     
     
     return (
         <div className={s.timerWindow}>
-            <div>
-                
-           <input type='radio' />
+            <div className={s.switchWrapper}>
+               {switchCheck}
             </div>
 
             <button onClick={onClick}>Start</button>
