b1bd9a4d1f36e6bb8aef5c522746aa46a1f40131
SergeyDubrovski <sovelectron@rambler.ru>
2025-01-18 09:59:08 +0300

    17.01

--- src/home/TimerWindow.tsx
+++ src/home/TimerWindow.tsx
@@ -1,7 +1,8 @@
-import  { useState } from 'react'
+import { useState } from 'react'
 
 import s from './home.module.css'
 import { Switch } from '@mui/material'
+import { LuBadgeX } from "react-icons/lu";
 
 type Props = {
     turnOnOff: (onOff: boolean) => void
@@ -12,44 +13,45 @@ type Props = {
 function TimerWindow({ turnOnOff, showTimer }: Props) {
 
 
-const [time, setTime] = useState<Array<boolean>>([
-    false, false, true, false, false
-])
-const [handleTime, setHandleTime] = useState<number>(1)
+    const [time, setTime] = useState<Array<boolean>>([
+        false, false, true, false, false
+    ])
+    const [handleTime, setHandleTime] = useState<number>(1)
     const onClick = () => {
         console.log(handleTime);
-        
+
         turnOnOff(true)
         showTimer()
     }
-  const timeString: Array<string> = ['1 min', '5 min', '10 min', '30 min', 'forever']  
+    const timeString: Array<string> = ['1 min', '5 min', '10 min', '30 min', 'forever']
 
-  const onChange = (index:number) => {
-    setHandleTime(index)
-    setTime(
-        prev => prev.map((_, i) => {
-            if(i === index) return true
-            return false
-        })
-    )
-  }
-  
+    const onChange = (index: number) => {
+        setHandleTime(index)
+        setTime(
+            prev => prev.map((_, i) => {
+                if (i === index) return true
+                return false
+            })
+        )
+    }
+
 
     const switchCheck: JSX.Element[] = timeString.map((el, i) => {
         return (
             <div key={el}>
-                    <Switch checked={time[i] ? true : false} color="warning" 
-                    onChange={() => {onChange(i)}}/>
-                    <span>{el}</span>
-                </div>
+                <Switch checked={time[i] ? true : false} color="warning"
+                    onChange={() => { onChange(i) }} />
+                <span>{el}</span>
+            </div>
         )
     })
-    
-    
+
+
     return (
         <div className={s.timerWindow}>
+            <LuBadgeX onClick={showTimer} className={s.iClose} size={40}/>
             <div className={s.switchWrapper}>
-               {switchCheck}
+                {switchCheck}
             </div>
 
             <button onClick={onClick}>Start</button>
