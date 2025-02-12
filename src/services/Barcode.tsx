335e93bc738e5fc9bfc5d5f27a7149f842a6fc52
SergeyDubrovski <sovelectron@rambler.ru>
2025-02-12 15:46:29 +0300

    12.02.14

--- src/services/Barcode.tsx
+++ src/services/Barcode.tsx
@@ -4,13 +4,14 @@ import BarcodeScannerComponent from "react-qr-barcode-scanner";
 export const Barc = () => {
     const [data, setData] = useState("Not Found");
     const [stop, setStop] = useState<boolean>(false)
+    const [flash, setFlash] = useState<boolean>(false)
    return (
     <>
     <BarcodeScannerComponent
     delay={3000}
       width={400}
       height={200}
-      torch={true}
+      torch={flash}
       stopStream={stop}
       onUpdate={(err, result: any) => {
         if (result) {setData(result.text);
@@ -22,6 +23,8 @@ export const Barc = () => {
       }}
     />
     <p>{data}</p>
+    <button style={{width:'100px', height:'50px'}} onClick={() => setFlash(prev => !prev )}>FLASH</button>
+    <button style={{width:'100px', height:'50px'}} onClick={() => setStop(false)}>RELOAD</button>
   </>)
 };
 
