2bbd0fbf4ef3b0062264de5cb25c49480589a635
SergeyDubrovski <sovelectron@rambler.ru>
2025-02-13 21:56:18 +0300

    12.02.14

--- src/barcode/Barcode2.tsx
+++ src/barcode/Barcode2.tsx
@@ -29,7 +29,7 @@ const res = result.map((el, i) => {
     <div>
          <Html5QrcodePlugin2
          fps={10}
-         qrbox={{width: 200, height: 60}}
+         qrbox={{width: 220, height: 20}}
          
          showTorchButtonIfSupported={true}
          qrCodeSuccessCallback={onNewScanResult}
