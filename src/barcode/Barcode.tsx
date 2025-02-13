8717039e37d41889aca7c6f8f4a5e61885050178
SergeyDubrovski <sovelectron@rambler.ru>
2025-02-13 14:14:08 +0300

    12.02.14

--- src/barcode/Barcode.tsx
+++ src/barcode/Barcode.tsx
@@ -1,43 +1,12 @@
 
-import { QrcodeSuccessCallback } from 'html5-qrcode'
+
 import Html5QrcodePlugin from './HTML';
-import { useState } from 'react';
+
 
 
 function Barcode() {
 
-     const [result, setResult] = useState<string[]>(['Wait!!!']) 
-
-const onNewScanResult:QrcodeSuccessCallback = (decodedText, decodedResult) => {
-
-    setResult((prev:string[]):string[] => {
-      if(prev[0] === 'Wait!!!') return [decodedText]
-      if (prev[prev.length - 1] === decodedText) return prev
-      return [...prev, decodedText]
-    })
-    console.log(decodedText, decodedResult);
-    
-    // handle decoded results here
-};
-const res = result.map((el, i) => {
-  return(
-    <h6 key={el + i}>{el}</h6>
-  )
-})
-
-  return (
-    <div>
-         <Html5QrcodePlugin
-         fps={10}
-         qrbox={{width: 200, height: 60}}
-         
-         showTorchButtonIfSupported={true}
-         qrCodeSuccessCallback={onNewScanResult}
-        />
-        <h1>{res}</h1>
-    </div>
-   
-  )
+   <Html5QrcodePlugin />
 }
 
 export default Barcode
