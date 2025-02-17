5e2cad78484b22fff19014a1c2f8d0b1a79403fe
SergeyDubrovski <sovelectron@rambler.ru>
2025-02-17 13:30:45 +0300

    12.02.14

--- src/barcode/Barcode.tsx
+++ src/barcode/Barcode.tsx
@@ -40,7 +40,7 @@ return (
             width: '100%',
             height: '2px',
             backgroundColor: 'red',
-            transform: 'translateY(-50%)',
+            //transform: 'translateY(-50%)',
           }}
         />
       )
