1c78d3b959671be265f617df2ba46e0b6125ada7
SergeyDubrovski <sovelectron@rambler.ru>
2025-02-17 12:23:14 +0300

    12.02.14

--- src/barcode/Barcode.tsx
+++ src/barcode/Barcode.tsx
@@ -1,9 +1,48 @@
 
-import HtML from './HTML';
+import React, { useState } from 'react';
+import BarcodeScannerComponent from 'react-qr-barcode-scanner';
 
+const Barcode: React.FC = () => {
+  const [data, setData] = useState<string>('No result');
+  const [scanning, setScanning] = useState<boolean>(true);
 
-function Barcode() {
-return <HtML />
-}
+  return (
+    <div style={{ position: 'relative', width: '100%', maxWidth: '500px', margin: 'auto' }}>
+      {/* Barcode Scanner Component */}
+      <BarcodeScannerComponent
+        width="100%"
+        height="100%"
+        onUpdate={(err, result:any) => {
+          if (result) {
+            setData(result.text);
+            setScanning(false); // Stop scanning after a result is found
+          } else {
+            setData('No result');
+          }
+          if(err) console.log(err);
+          
+        }}
+      />
 
-export default Barcode
+      {/* Red Line to Indicate Scanning Area */}
+      {scanning && (
+        <div
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: '0',
+            width: '100%',
+            height: '2px',
+            backgroundColor: 'red',
+            transform: 'translateY(-50%)',
+          }}
+        />
+      )}
+
+      {/* Display Scanned Data */}
+      <p>{data}</p>
+    </div>
+  );
+};
+
+export default Barcode;
--- src/barcode/HTML.tsx
+++
@@ -1,7 +0,0 @@
-import { BarcodeScanner } from 'react-barcode-scanner'
-import "react-barcode-scanner/polyfill"
-
-const HtML = () => {
-  return <BarcodeScanner />
-}
-export default HtML
