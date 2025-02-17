bc8640ea2fd83309f9a6c2bfc39ad236a63cdee2
SergeyDubrovski <sovelectron@rambler.ru>
2025-02-17 12:29:16 +0300

    12.02.14

--- src/barcode/Barcode.tsx
+++ src/barcode/Barcode.tsx
@@ -4,7 +4,7 @@ import BarcodeScannerComponent from 'react-qr-barcode-scanner';
 
 const Barcode: React.FC = () => {
   const [data, setData] = useState<string>('No result');
-  const [scanning, setScanning] = useState<boolean>(true);
+  //const [scanning, setScanning] = useState<boolean>(true);
 
   return (
     <div style={{ position: 'relative', width: '100%', maxWidth: '500px', margin: 'auto' }}>
@@ -15,7 +15,7 @@ const Barcode: React.FC = () => {
         onUpdate={(err, result:any) => {
           if (result) {
             setData(result.text);
-            setScanning(false); // Stop scanning after a result is found
+            //setScanning(false); // Stop scanning after a result is found
           } else {
             setData('No result');
           }
@@ -25,7 +25,7 @@ const Barcode: React.FC = () => {
       />
 
       {/* Red Line to Indicate Scanning Area */}
-      {scanning && (
+      
         <div
           style={{
             position: 'absolute',
@@ -37,8 +37,7 @@ const Barcode: React.FC = () => {
             transform: 'translateY(-50%)',
           }}
         />
-      )}
-
+      )
       {/* Display Scanned Data */}
       <p>{data}</p>
     </div>
