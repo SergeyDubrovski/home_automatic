ee9be6cdbccb4318d74534572ae312c882fdd33d
SergeyDubrovski <sovelectron@rambler.ru>
2025-02-20 10:01:59 +0300

    12.02.14

--- src/barcode/Barcode2.tsx
+++ src/barcode/Barcode2.tsx
@@ -2,13 +2,11 @@
 import { QrcodeSuccessCallback } from 'html5-qrcode'
 import { useState } from 'react';
 import Html5QrcodePlugin2 from './Html5Qr';
-import { QrDimensions } from 'html5-qrcode/esm/core';
 
 
 function Barcode2() {
 
      const [result, setResult] = useState<string[]>(['Wait!!!']) 
-     const [decoded, setDecoded] =useState<any>('')
 
 const onNewScanResult:QrcodeSuccessCallback = (decodedText, decodedResult) => {
 
@@ -18,21 +16,9 @@ const onNewScanResult:QrcodeSuccessCallback = (decodedText, decodedResult) => {
       return [...prev, decodedText]
     })
     console.log(decodedText, decodedResult);
-    setDecoded(decodedResult.result.format?.formatName)
+    
     // handle decoded results here
 };
-type QrDimensionFunction =
-    (viewfinderWidth: number, viewfinderHeight: number) => QrDimensions;
-
-    let qrboxFunction:QrDimensionFunction = function(viewfinderWidth, viewfinderHeight) {
-      let minEdgePercentage = 0.7; // 70%
-      let minEdgeSize = Math.min(viewfinderWidth, viewfinderHeight);
-      let qrboxSize = Math.floor(minEdgeSize * minEdgePercentage);
-      return {
-          width: qrboxSize,
-          height: qrboxSize
-      };
-  }
 const res = result.map((el, i) => {
   return(
     <h6 key={el + i}>{el}</h6>
@@ -42,14 +28,13 @@ const res = result.map((el, i) => {
   return (
     <div>
          <Html5QrcodePlugin2
-         fps={100}
-         qrbox={qrboxFunction}
+         fps={300}
+         qrbox={{width: 170, height: 100}}
          
          showTorchButtonIfSupported={true}
          qrCodeSuccessCallback={onNewScanResult}
         />
         <h1>{res}</h1>
-        <h2>{decoded}</h2>
     </div>
    
   )
